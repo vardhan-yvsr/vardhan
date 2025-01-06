3cf71f4ca5417dc9741e5ef9603f3660a12c0948
vardhan-yvsr <yashlkw2003@gmail.com>
2025-01-06 17:10:10 +0530

    3

--- assets/js/index.js
+++ assets/js/index.js
@@ -107,7 +107,7 @@ function cvCommand(){
   printContent(commands['phone']);
   printContent(commands['github']);
   printContent(commands['linkedIn']);
-  // printContent(commands['resume']);
+  printContent(commands['resume']);
   insertLine("\n<strong>--Experience--</strong>\n",true);
   printContent(commands['experience']);
   insertLine("\n<strong>--Education--</strong>\n",true);
