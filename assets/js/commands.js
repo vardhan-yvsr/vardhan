1e83ef71ba61b87bc4a5ff7a172122716900ef5b
vardhan-yvsr <yashlkw2003@gmail.com>
2025-01-06 16:52:00 +0530

    first commit

---
+++ assets/js/commands.js
@@ -0,0 +1,94 @@
+const commands = {
+  "whoiam":{
+    "description":"Tells you who i am",
+    "multiline":false,
+    "content":"Hello! I'm Yash Vardhan Singh and I'm a B.Tech. Final year AI-ML student, From Galgotias College of Engineering and Technology. I Leverage data analytics expertise to extract actionable insights from large datasets and implement Artificial Intelligence and Machine Learning algorithms to solve complex problems."
+  },
+  "mail":{
+    "description":"Get my mail",
+    "multiline":false,
+    "content":"My Email: yashlkw2003@gmail.com"
+  },
+  "phone":{
+    "description":"Get my Phone Number",
+    "multiline":false,
+    "content":"My Phone Number: 9839658555"
+  },
+  "github":{
+    "description":"Get my github profile",
+    "multiline":false,
+    "content":"I have a github profile which you can see at the link https://github.com/vardhan-yvsr"
+  },
+  "linkedin":{
+    "description":"Get my Linkedin profile",
+    "multiline":false,
+    "content":"I have a Linkedin profile which you can see at the link https://www.linkedin.com/in/yash-yvsr/"
+  },
+  "education":{
+    "description":"Get my education timeline",
+    "multiline":true,
+    "content":[
+      "Place: Galgotias College of Engineering and Technology  2021-Now  -  Degree: B.Tech. Artificial Intelligence and Machine Learning",
+      "Place: Montfort Inter College  2019-2021  -  Degree: Intermediate - Percentage: 93%",
+      "Place: Montfort Inter College  2007-2019  -  Degree: High School - Percentage: 95%"
+    ]
+  },
+  "experience":{
+    "description":"Get my experience timeline",
+    "multiline":true,
+    "content":[
+      "Outlier.ai | Freelance AI Specialist | Remote | November 2024 – Present",
+      "Conducting RLHF (Reinforcement Learning with Human Feedback) training to optimize language models for AI applications.",
+      "Specializing in training AI models for programming languages, enhancing capabilities in coding logic, efficiency, and error handling.",
+      "Collaborating on AI training projects to enhance model accuracy and robustness.",
+      " ",
+      " ",
+      "DataAnnotation.tech | Data Analyst (Generative AI) | Remote July 2024 - October 2024",
+      "Trained Hindi Conversational AI models, improving response relevance by 25% for native language users.",
+      "Contributed to the development of Claude AI, enhancing conversation flow and intent recognition by 20% through AI training."
+    ]
+  },
+  "projects":{
+    "description":"Get cool projects I created.",
+    "multiline":true,
+    "content":[
+      "Power BI Dashboard Project: Streaming Platform Analysis",
+      "Designed and developed an interactive OTT Dashboard for Netflix, Amazon Prime, and Disney+ Hotstar using Power BI.",
+      "Built dynamic visualizations for analyzing platform-wise content distribution and ratings.",
+      "Achieved a comprehensive, user-friendly dashboard that improved OTT content analysis by 40%. ",
+      "Link : https://drive.google.com/file/d/17idH_vpmEBzav1GnM9kLRXIJYUHgr1r7/view?usp=sharing",
+    ]
+  },
+  "skills":{
+    "description":"See my Skills.", 
+    "multiline":true,
+    "content":[
+      "Frontend Development - HTML, CSS, JavaScript.",
+      "Backend Development - Database Management :MySQL, MongoDB.",
+      "AI and ML - Python ,TensorFlow, PyTorch, XGBoost, Keras, and OpenCV.",
+      "Data Visualization Tools - Power BI, Matplotlib, Seaborn.",
+      "Cybersecurity Tools - Wireshark, Metasploit, Nessus, Burp Suite, Nmap.",
+      "Operating Systems - Linux (Kali, Ubuntu), Windows (Server and Desktop).",
+      "Version Control - Git, GitHub"
+    ]
+  },
+  "certifications":{
+    "description":"See my Certifications and Achievements.", 
+    "multiline":true,
+    "content":[
+      "Google Cybersecurity Specialization, Google | Coursera | Jun, 2023 ",
+      "Completed an 8-course program by Google, gaining hands-on skills in Python, Linux, SQL, SIEM tools, and Intrusion Detection Systems (IDS).",
+      "Link : https://coursera.org/share/a631e50a224e7011bb7f13160ff1bebb",
+      " ",
+      " ",
+      "Google IT Support , Google | Coursera | Sept , 2022 ",
+      "Completed a 5-course specialization by Google, gaining hands-on expertise in troubleshooting, customer service, networking, operating systems, system administration, and IT security.",
+      "Link : https://coursera.org/share/4c5fba9209b18ef3d34a59d56ffb413d",
+      " ",
+      " ",
+      "Participant, Kavach 2023 CyberSecurity Hackathon:",
+      "Worked on the Obscenity Blocker Solution, developing a system to detect and block inappropriate content effectively.",
+    ]
+  }
+
+}
